5a445c75b34240fcb89d8c93e1fa401136f99363
Ammar Montaser <me@ammarmontaser.com>
2024-07-07 23:20:32 +0300

    added export to csv button next to  search bar and added headers to it

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,13 @@
 "use client";
 
 import useSWR from "swr";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { Event } from "./core/types/types";
 import NormalTableRow from "@/components/normalTableRow";
 import ExpandedTable from "@/components/expandedTableRow";
 import LoadingSkeleton from "@/components/loadingSkeleton";
-
-// import { useCallback } from "react";
-// import { saveAs } from "file-saver";
+import Export from "./core/export";
+import saveAs from "file-saver";
 
 export default function Home() {
   const fetcher = (...args: Parameters<typeof fetch>) =>
@@ -23,23 +22,24 @@ export default function Home() {
     fetcher
   );
 
-  // <<<<<<<<<<<<<CSV export feature>>>>>
-  // const exportToCSV = useCallback(async () => {
-  //   const response = await fetch(`/api/events?search=${search}`);
-  //   const events = await response.json();
-  //   const csvData = events.events.map((event: Event) =>
-  //     Object.values(event).join(",")
-  //   );
-  //   const blob = new Blob([csvData.join("\n")], { type: "text/csv" });
-  //   const fileName = "events.csv";
-  //   saveAs(blob, fileName);
-  // }, [search]);
+  const exportToCSV = useCallback(async () => {
+    const response = await fetch(`/api/events?search=${search}`);
+    const events = await response.json();
+    const csvHeaders = Object.keys(events.events[0]).join(",");
+    const csvData = [
+      csvHeaders,
+      ...events.events.map((event: Event) => Object.values(event).join(",")),
+    ].join("\n");
+    const blob = new Blob([csvData], { type: "text/csv" });
+    const fileName = "events.csv";
+    saveAs(blob, fileName);
+  }, [search]);
 
   return (
     <main className="flex flex-col min-h-screen">
       <div className="flex flex-col justify-between m-auto w-[933px] min-h-[743px]  bg-[#ffff] rounded-[15px] border border-[#F0F0F0] ">
         <div className="flex flex-col w-full h-[108px]  bg-[#F5F5F5]  rounded-t-[15px]">
-          <div className="  flex flex-row justify-center">
+          <div className="  flex flex-row  justify-start items-center m-auto">
             <input
               name="search"
               type="text"
@@ -48,8 +48,18 @@ export default function Home() {
                 setSearch(e.target.value);
               }}
               placeholder="Search name, email or action..."
-              className="w-[899px] h-[45px] mt-4 px-4 bg-[#F5F5F5] border border-[#E0E0DF] hover:border-[#bebebe] rounded-[8px] m-auto"
+              className="w-[800px] h-[45px] mt-4 px-4 bg-[#F5F5F5] border border-[#E0E0DF]  rounded-l-[8px] m-auto"
             />
+
+            <button
+              className="flex h-[45px] justify-center items-center mt-4 px-4  border-t border-b border-r rounded-r-[8px] border-[#E0E0DF]"
+              onClick={exportToCSV}
+            >
+              <Export />
+              <p className="text-[12px] font-normal text-[#575757] mt-1">
+                EXPORT
+              </p>
+            </button>
           </div>
           <div className="flex flex-row h-[47px] w-[899px] items-center justify-center m-auto ">
             <h6 className="text-[#616161] w-1/3  text-[14px] font-semibold">
@@ -74,7 +84,7 @@ export default function Home() {
               </tr>
             )}
             {isLoading &&
-              Array.from({ length: limit }, (_, index) => (
+              Array.from({ length: 11 }, (_, index) => (
                 <LoadingSkeleton key={index} />
               ))}
             {data &&
@@ -110,11 +120,6 @@ export default function Home() {
           >
             LOAD MORE
           </button>
-          {/* 
-          <<<<<<<<<<<<<<<<<CSV export Button Test>>>>>>>>>>>>>>>
-          <button className="text-[#616161] ml-[100px]" onClick={exportToCSV}>
-            EXPORT TO CSV
-          </button> */}
         </div>
       </div>
     </main>
